8a0ff7c876be1ed9b6fbf9ce97a2f91680082949
Jennifer Power <barnabei.jennifer@gmail.com>
2026-01-06 18:41:19 -0500

    chore(deps): bump to otter v2
    Otter v2 is the latest stable release
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- truthbeam/internal/client/otter.go
+++ truthbeam/internal/client/otter.go
@@ -3,7 +3,8 @@ package client
 import (
 	"time"
 
-	"github.com/maypok86/otter"
+	"github.com/maypok86/otter/v2"
+	"github.com/maypok86/otter/v2/stats"
 )
 
 // Interface Check
@@ -11,27 +12,29 @@ var _ Cache = (*otterCacheStore)(nil)
 
 // otterCacheStore implements Cache using Otter.
 type otterCacheStore struct {
-	cache otter.Cache[string, Compliance]
+	cache *otter.Cache[string, Compliance]
 }
 
 func (s *otterCacheStore) Get(key string) (Compliance, bool) {
-	return s.cache.Get(key)
+	return s.cache.GetIfPresent(key)
 }
 
 func (s *otterCacheStore) Set(key string, value Compliance) error {
-	s.cache.Set(key, value)
+	_, _ = s.cache.Set(key, value)
 	return nil
 }
 
 func (s *otterCacheStore) Delete(key string) error {
-	s.cache.Delete(key)
+	_, _ = s.cache.Invalidate(key)
 	return nil
 }
 
 func NewOtterStore(ttl time.Duration, maxEntries int) (Cache, error) {
-	cache, err := otter.MustBuilder[string, Compliance](maxEntries).
-		CollectStats().
-		WithTTL(ttl).
-		Build()
-	return &otterCacheStore{cache: cache}, err
+	opts := &otter.Options[string, Compliance]{
+		MaximumSize:    maxEntries,
+		ExpiryCalculator: otter.ExpiryWriting[string, Compliance](ttl),
+		StatsRecorder:  stats.NewCounter(),
+	}
+	cache := otter.Must(opts)
+	return &otterCacheStore{cache: cache}, nil
 }
